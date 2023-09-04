cf0fe46515d1006dcf2236813ef4ee2ea1216184
d.ridniy <allbegj@gmail.com>
2023-09-04 10:51:56 +0500

    homework 8 fix

---
+++ my-project/src/components/IndexPage.vue
@@ -0,0 +1,172 @@
+﻿<template>
+    <body>
+        <div class="container-stripes">
+            <header class="site-header center">
+                <a href="/" class="site-header__link-logo">
+                    <img src="img/site-logo.svg" alt="site logo" class="site-logo site-logo_header">
+                </a>
+                <ul class="site-header__link-list">
+                    <li class="site-header__link-item">
+                        <a href="">Home</a>
+                    </li>
+                    <li class="site-header__link-item">
+                        <a href="">Project</a>
+                    </li>
+                    <li class="site-header__link-item">
+                        <a href="">Blog</a>
+                    </li>
+                </ul>
+            </header>
+        </div>
+        <div class="container-stripes">
+            <section class="main-banner center">
+                <h1 class="main-banner__title">Let Your Home Be Unique</h1>
+                <div class="main-banner__subtitle">There are many variations of the passages of
+                    lorem Ipsum fromavailable, majority.</div>
+                <a href="" class="button">Get Started</a>
+            </section>
+        </div>
+        <div class="container-stripes">
+            <main class="our-projects center">
+                <h2 class="our-projects__title">Follow Our Projects</h2>
+                <div class="our-projects__subtitle">It is a long established fact that a reader will be distracted by the of
+                    readable content of page lookings at its layouts points.</div>
+                <div class="our-projects__catalog">
+                    <div class="product-card">
+                        <img src="img/our-projects_item-1.png" alt="priduct card image" class="product-card__image">
+                        <div class="product-card__title">Modern Kitchan</div>
+                        <div class="product-card__subtitle">Decor / Artchitecture</div>
+                        <a href="" class="round-button round-button_our-projects"></a>
+                    </div>
+                    <div class="product-card">
+                        <img src="img/our-projects_item-2.png" alt="priduct card image" class="product-card__image">
+                        <div class="product-card__title">Modern Kitchan</div>
+                        <div class="product-card__subtitle">Decor / Artchitecture</div>
+                        <a href="" class="round-button round-button_our-projects"></a>
+                    </div>
+                    <div class="product-card">
+                        <img src="img/our-projects_item-3.png" alt="priduct card image" class="product-card__image">
+                        <div class="product-card__title">Modern Kitchan</div>
+                        <div class="product-card__subtitle">Decor / Artchitecture</div>
+                        <a href="" class="round-button round-button_our-projects"></a>
+                    </div>
+                    <div class="product-card">
+                        <img src="img/our-projects_item-4.png" alt="priduct card image" class="product-card__image">
+                        <div class="product-card__title">Modern Kitchan</div>
+                        <div class="product-card__subtitle">Decor / Artchitecture</div>
+                        <a href="" class="round-button round-button_our-projects"></a>
+                    </div>
+                </div>
+            </main>
+        </div>
+        <div class="container-stripes">
+            <section class="resons center">
+                <div class="resons__item">
+                    <div class="resons__number">12</div>
+                    <div class="resons__desc">Years Of Experiance</div>
+                </div>
+                <div class="resons__item">
+                    <div class="resons__number">85</div>
+                    <div class="resons__desc">Success Project</div>
+                </div>
+                <div class="resons__item">
+                    <div class="resons__number">15</div>
+                    <div class="resons__desc">Active Project</div>
+                </div>
+                <div class="resons__item">
+                    <div class="resons__number">95</div>
+                    <div class="resons__desc">Happy CUstomers</div>
+                </div>
+            </section>
+        </div>
+        <div class="container-stripes">
+            <section class="articles-news center">
+                <h2 class="articles-news__title">Articles & News</h2>
+                <div class="articles-news__subtitle">It is a long established fact that a reader will be distracted by the
+                    of readable content of a page when lookings at its layouts the points of using.</div>
+                <div class="articles-news__content">
+                    <div class="news-card">
+                        <div class="news-card__image-wrapp">
+                            <img src="img/articles-news_item-1.png" alt="news card image" class="news-card__image">
+                            <div class="news-card__genre">Kitchan Design</div>
+                        </div>
+                        <div class="news-card__title">Let’s Get Solution For Building Construction Work</div>
+                        <div class="news-card__date">26 December,2022 </div>
+                        <a href="" class="round-button round-button_articles-news"></a>
+                    </div>
+                    <div class="news-card news-card__beige">
+                        <div class="news-card__image-wrapp">
+                            <img src="img/articles-news_item-2.png" alt="news card image" class="news-card__image">
+                            <div class="news-card__genre">Living Design</div>
+                        </div>
+                        <div class="news-card__title">Low Cost Latest Invented Interior Designing
+                            Ideas.</div>
+                        <div class="news-card__date">22 December,2022 </div>
+                        <a href="" class="round-button round-button_articles-news round-button_articles-news-beige"></a>
+                    </div>
+                    <div class="news-card">
+                        <div class="news-card__image-wrapp">
+                            <img src="img/articles-news_item-3.png" alt="news card image" class="news-card__image">
+                            <div class="news-card__genre">Interior Design</div>
+                        </div>
+                        <div class="news-card__title">Best For Any Office & Business Interior
+                            Solution</div>
+                        <div class="news-card__date">25 December,2022 </div>
+                        <a href="" class="round-button round-button_articles-news"></a>
+                    </div>
+                </div>
+            </section>
+        </div>
+        <div class="container-stripes">
+            <footer class="site-footer center">
+                <div class="site-footer__column site-footer__column-1">
+                    <img src="img/site-logo.svg" alt="site logo" class="site-logo site-logo_footer">
+                    <div class="site-footer__tagline">It is a long established fact that a reader will be distracted
+                        lookings.</div>
+                    <div class="site-footer__social">
+                        <img src="img/twitter-icon.svg" alt="twitter icon">
+                        <img src="img/in-icon.svg" alt="in icon">
+                    </div>
+                </div>
+                <div class="site-footer__column site-footer__column-2">
+                    <div class="site-footer__list-title">Pages</div>
+                    <ul class="site-footer__list-links">
+                        <li class="site-footer__item-link"><a href="">Home</a></li>
+                        <li class="site-footer__item-link"><a href="">Project</a></li>
+                        <li class="site-footer__item-link"><a href="">Blog</a></li>
+                    </ul>
+                </div>
+                <div class="site-footer__column site-footer__column-3">
+                    <div class="site-footer__contacts-title">Contact</div>
+                    <div class="site-footer__address">55 East Birchwood Ave. Brooklyn, New York 11201</div>
+                    <a href="" class="site-footer__mail">contact@interno.com</a>
+                    <a href="tel:+71234567890" class="site-footer__phone">(123) 456 - 7890</a>
+                </div>
+            </footer>
+        </div>
+    </body>
+</template>
+
+<script>
+export default {
+    name: 'IndexPage',
+
+    data() {
+        return {
+
+        };
+    },
+
+    mounted() {
+
+    },
+
+    methods: {
+
+    },
+};
+</script>
+
+<style lang="sass" scoped>
+
+</style>
